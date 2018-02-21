e7c781cb1a19dfb3993a55a743d577a504ad8783
root <zhidkob.gleb@gmail.com>
2018-02-22 01:26:29 +0300

    One Line Solution. ez get good job

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    for (const br of str) {
+        const pair = bracketsConfig.find(b => b.includes(br));
+            br === pair[1] && pair[0] === stack[stack.length - 1] ? stack.pop() : stack.push(br);
+    }
+    return !stack.length;
+    //OLOLO PERL STYLE
+    // return str.split('').reduce((accum,br) =>  ((br === bracketsConfig.find(b => b.includes(br))[1] && bracketsConfig.find(b => b.includes(br))[0] === accum[accum.length - 1] ? accum.pop() :  accum.push(br)),true) && accum,[]).length;
 }
+
+
+
